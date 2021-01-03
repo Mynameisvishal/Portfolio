52632cda5794d52b212892f84699f7ad6e9cb895
Vishal P <vishalprakaash@gmail.com>
2021-01-04 00:54:43 +0530

    Initial setup

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+           <div className="header__brandname">
+               header
+           </div>
+        </div>
+    )
+}
+
+export default Header
