29264c05bc1464f0535806a29e6fb94144a9164e
Vishal P <vishalprakaash@gmail.com>
2021-01-04 20:42:02 +0530

    added responsive sidebar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Header.css';
 
 function Header() {
+
+    const [click, setClick] = useState(false);
+
     return (
         <div className="header__nav">
            <div className="header__brandname">
                Vishal Prakash
            </div>
-           <div className="header__sidemenu">
+           <div className="header__menu">
                <button>Home</button>
                <button>About</button>
                <button>Service</button>
                <button>Works</button>
                <button>Contact</button>
+           </div>
+           <div onClick={()=>setClick(!click)} className={`header__sidemenu ${click && "show"}`}>
+               <div className="header__line"></div>
+               <div className="header__line"></div>
+               <div className="header__line"></div>
            </div>
         </div>
     )
